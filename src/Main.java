6fdafaea6d50f9c7b3e4c095fb1c3ea256bbf9fd
mismusen <32872317+mismusen@users.noreply.github.com>
2018-11-19 14:07:35 +0100

    d

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        //stopppppp
-        //Stooop
-        //stop
-        //Has you stopped?
-        // NO
     }
 }

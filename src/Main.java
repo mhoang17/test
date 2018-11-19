64b6506165cd7c6eeadd9ff11b1cd0b257a91df0
mhoang17 <mhoang17@github.com>
2018-11-19 14:15:19 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Work");
         System.out.println("Stupid");
+        System.out.println("Intellij");
     }
 }
